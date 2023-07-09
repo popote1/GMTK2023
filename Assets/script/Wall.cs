83538b267aaa19be51723a922154b01e62677549
popotePC2 <yvan.bladet@gmail.com>
2023-07-09 13:01:22 +0200

    Probs Integration

--- Assets/script/Wall.cs
+++ Assets/script/Wall.cs
@@ -6,6 +6,7 @@ namespace script
     public class Wall : MonoBehaviour, IDestructible
     {
         public GameObject prefabsDebrie;
+        public GameObject prefabsFX;
         public int HP;
 
         public void TakeDamage(int damage)
@@ -20,6 +21,7 @@ namespace script
 
         public void Destroy()
         {
+            if (prefabsFX) Instantiate(prefabsFX, transform.position, transform.rotation);
             if (prefabsDebrie) Instantiate(prefabsDebrie, transform.position, transform.rotation);
             Destroy(gameObject);
         }
